c3c96f50ab1a368d2f2b44a44a6f3d1598a75827
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2022-04-04 18:00:31 +0100

    +

--- part2.js
+++ part2.js
@@ -7,7 +7,21 @@
 // });
 
 class Timer {
-    constructor() {
+    constructor(durationInput, startButton, pauseButton) {
+        this.durationInput = durationInput;
+        this.startButton = startButton;
+        this.pauseButton = pauseButton;
 
+        this.startButton.addEventListener('click', this.start);
+    }
+
+    start() {
+        console.log('TIme To start Timer');
     }
 }
+
+const durationInput = document.querySelector('#duration');
+const startButton = document.querySelector('#start');
+const pauseButton = document.querySelector('#pause');
+
+const timer = new Timer(durationInput, startButton, pauseButton);
