e2fe0b80810bfadce3badba7869dc85486b243b3
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2022-04-05 20:17:15 +0100

    +

--- part2.js
+++ part2.js
@@ -29,4 +29,10 @@ const startButton = document.querySelector('#start');
 const pauseButton = document.querySelector('#pause');
 
 const timer = new Timer(durationInput, startButton, pauseButton);
-timer.start(); 
+// timer.start(); 
+
+// console.log(this);
+// const printThis = () => {
+//     console.log(this);
+// };
+// printThis();
