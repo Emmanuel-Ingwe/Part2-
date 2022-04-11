d5a12513e8786180484a3e1aabc5e2dbf4317161
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2022-04-11 20:39:19 +0200

    addin values

--- part2.js
+++ part2.js
@@ -13,12 +13,12 @@ class Timer {
         this.pauseButton = pauseButton;
 
         this.startButton.addEventListener('click', this.start);
+        this.pauseButton.addEventListener('click', this.pause);
     }
 
     start = () => {
         this.tick();
-        const timer = setInterval(this.tick, 1000);
-        clearInterval(timer);
+        this.interval = setInterval(this.tick, 1000);
     };
 
     pause = () => {
