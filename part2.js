231fdad131aaf253e205160bb2fd74dba7626c0a
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2022-04-12 15:30:28 +0200

    +

--- part2.js
+++ part2.js
@@ -26,32 +26,20 @@ class Timer {
     };
 
     tick = () => {
-        console.log('tick');
+        const timeRemaining = parseFloat(this.durationInput.value);
+        this.timeRemaining = this.timeRemaining - 1;
     };
+
+    get timeRemaining() {
+        return parseFloat(this.durationInput.value);
+    }
+
+    set timeRemaining(time) {
+        this.durationInput.value = time;
+    }
 }
 
 const durationInput = document.querySelector('#duration');
 const startButton = document.querySelector('#start');
 const pauseButton = document.querySelector('#pause');
 const timer = new Timer(durationInput, startButton, pauseButton);
-
-const colors = {
-    printColor() {
-        console.log(this);
-    }
-};
-const randomObject = {
-    a: 1
-};
-randomObject.printColor = colors.printColor;
-randomObject.printColor();
-
-start = () => {
-    this.tick();
-    this.timer = setInterval(this.tick, 1000);
-};
-
-
-tick = () => {
-    console.log('tick');
-};
